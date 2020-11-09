8fd110fadbfca23f206135b6f6d5b545843dcdc8
Chris <tobarchr@gmail.com>
2020-11-09 14:10:12 -0500

    first version

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
+function App() {
+  const [pokemon,setPokemon] = useState([]);
+
+    const onClick = () =>{
+      fetch('https://pokeapi.co/api/v2/pokemon?limit=808')
+        .then(response => response.json())
+        .then(response => setPokemon(response.results))
+    }
+  return (
+    <div className="container">
+        <div className="row justify-content-md-center">
+          <div class="col-md-auto">
+            <button className="btn btn-primary mt-2" onClick={onClick}>Fetch Pokemon</button>
+          </div>
+        </div>
+        <div className="row justify-content-md-center mt-2">
+          <div class="col-md-auto">
+            {pokemon.length>0 && pokemon.map((pokemon,index)=> {
+              return (
+                <ul className="list-group">
+                <li className="list-group-item d-flex justify-content-between align-items-center" key={index}>{pokemon.name}
+                <span className="badge badge-primary badge-pill">{index}</span>
+                </li>
+              </ul>
+              )
+            })}
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
